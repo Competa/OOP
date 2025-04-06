339c1a34851c6ab1f5b8e8cb0e8d233568591fe2
Nicholaj <competa123@hotmail.com>
2025-04-06 03:44:01 +0200

    changes

--- Exc/Chapter 5/Ch5Ex1/ConsoleApp1/Program.cs
+++ Exc/Chapter 5/Ch5Ex1/ConsoleApp1/Program.cs
@@ -3,13 +3,15 @@
     static void Main(string[] args)
     {
         short shortResult, shortVal = 4;
-        int integerValue = 67;
+        int integerValue = 67
         long longResult;
         float floatVal = 10.5F;
         double doubleResult, doubleVal = 99.999;
         string stringResult, stringVal = "17";
+        char charResult, charVal = 'A';
+        bool boolResult, boolVal = true;
 
-        
+
      
     }
 }
