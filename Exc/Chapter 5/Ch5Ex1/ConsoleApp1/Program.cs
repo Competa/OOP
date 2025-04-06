2cd341d6a7a5a4206eb0886063700067b01afdc1
Competa <competa123@hotmail.com>
2025-04-06 03:40:38 +0200

    commit

---
+++ Exc/Chapter 5/Ch5Ex1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        short shortResult, shortVal = 4;
+        int integerValue = 67;
+        long longResult;
+        float floatVal = 10.5F;
+        double doubleResult, doubleVal = 99.999;
+        string stringResult, stringVal = "17";
+
+        
+     
+    }
+}
