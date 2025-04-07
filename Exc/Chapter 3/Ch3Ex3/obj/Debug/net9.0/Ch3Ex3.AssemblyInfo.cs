10e33eac8fa30e68016026889bd9dfcbc179524d
Nicholaj Ghabour <competa123@hotmail.comm>
2025-04-07 17:37:16 +0200

    test

--- Exc/Chapter 3/Ch3Ex3/obj/Debug/net9.0/Ch3Ex3.AssemblyInfo.cs
+++ Exc/Chapter 3/Ch3Ex3/obj/Debug/net9.0/Ch3Ex3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ch3Ex3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff77c605d6d611a9974baba202434385c9d57bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ch3Ex3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ch3Ex3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
