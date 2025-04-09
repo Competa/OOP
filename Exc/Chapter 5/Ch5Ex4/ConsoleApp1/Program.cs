e5a62198ff3b6f15a76ac9291d45712d30f64e53
Nicholaj Ghabour <competa123@hotmail.com>
2025-04-09 21:17:31 +0200

    Ch5Ex4 now works.

--- Exc/Chapter 5/Ch5Ex4/ConsoleApp1/Program.cs
+++ Exc/Chapter 5/Ch5Ex4/ConsoleApp1/Program.cs
@@ -1,13 +1,18 @@
-﻿static void Main(string[] args)
-{
-string [] friendNames = {"Todd Anthony","John Doe","Jane Smith","Mary Johnson","James Brown"};
-int i;
-Console.WriteLine($"Here are {friendNames.Length} of my friends:");
-for (i = 0; i < friendNames.Length; i++)
-{
-    Console.WriteLine(friendNames[i]);
-}
-Console.ReadKey();
-}
+﻿using System;
 
-//The above code does not work.
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "John Doe", "Jane Smith", "Mary Johnson", "James Brown" };
+            Console.WriteLine($"Here are {friendNames.Length} of my friends:");
+            for (int i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine(friendNames[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Exc/Chapter 5/Ch5Ex4/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Exc/Chapter 5/Ch5Ex4/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54686d7c523299423f15e096649770e705ed3e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d30501762f8a8f09cb9148edf885452964e8079")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
