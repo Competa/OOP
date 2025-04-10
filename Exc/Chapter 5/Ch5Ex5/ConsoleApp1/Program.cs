0eeb6410cd414a307925df5130252900e3943c7e
Nicholaj Ghabour <competa123@hotmail.com>
2025-04-10 23:07:27 +0200

    added Ch05Ex05 - not finished

---
+++ Exc/Chapter 5/Ch5Ex5/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿static void Main(string[] args)
+{
+    string[] friendNames = { "Todd Anthony", "John Doe", "Jane Smith", "Mary Johnson", "James Brown" };
+    foreach (var friendNames in friendNames)
+    {
+        switch (friendNames)
+        {
+            case string t when t.StartsWith("T"):
+                Console.WriteLine($"{friendNames} starts with a 'T': "+ $""
+        }
+    }
+}
