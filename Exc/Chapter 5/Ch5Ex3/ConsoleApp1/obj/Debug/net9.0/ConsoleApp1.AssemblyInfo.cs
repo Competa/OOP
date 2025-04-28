9f07b06b06504d681b882bef81747fc2c0816cf2
Nicholaj <competa123@hotmail.com>
2025-04-28 21:42:20 +0200

    what happened

--- Exc/Chapter 5/Ch5Ex3/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Exc/Chapter 5/Ch5Ex3/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc54c88e6c2db2b4e8bec98242a970d79479406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4534ecf3c605de4f2b53c840a3f363ff9a264d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
