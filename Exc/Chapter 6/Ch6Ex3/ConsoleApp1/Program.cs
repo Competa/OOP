0f79cd24f860e48225a46d756bb98606efc35f6f
Nicholaj Ghabour <competa123@hotmail.com>
2025-04-30 22:31:05 +0200

    Added additional excercises.

---
+++ Exc/Chapter 6/Ch6Ex3/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int SumVals (params int[] vals)
+    {
+        int sum = 0;
+        foreach (int val in vals)
+        {
+            sum +=val;
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        int sum = SumVals (1,5,2,9,8,12,40);
+        Console.WriteLine($"Summed Values = {sum}");
+        Console.ReadKey();
+    }
+}
