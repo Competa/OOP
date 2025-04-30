b26028b26f2cc0e3fc1a4a538b74ff794b8cb741
Nicholaj Ghabour <competa123@hotmail.com>
2025-04-30 23:01:37 +0200

    merged with newest files

---
+++ Exc/Chapter 6/Ch6Ex11/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Write()
+    {
+        string myString = "Stgring defined in Write()";
+        Console.WriteLine("Now in Write()");
+        Console.WriteLine($"myString = {myString}");
+    }
+
+    static void Main(string[] args)
+    {
+        string myString = "String defined in Main()";
+        Write();
+        Console.ReadKey();
+    }
+}
