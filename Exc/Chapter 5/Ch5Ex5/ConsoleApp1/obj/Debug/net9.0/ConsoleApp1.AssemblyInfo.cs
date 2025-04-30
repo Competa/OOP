aec56f5391a44d38e27eddc5073b29dbcd70410f
Nicholaj Ghabour <competa123@hotmail.com>
2025-04-30 22:15:38 +0200

    Merged with windows.

--- Exc/Chapter 5/Ch5Ex5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Exc/Chapter 5/Ch5Ex5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4534ecf3c605de4f2b53c840a3f363ff9a264d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ccc12c44045d9c9f8f7bfce2611d4dafeb18ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
