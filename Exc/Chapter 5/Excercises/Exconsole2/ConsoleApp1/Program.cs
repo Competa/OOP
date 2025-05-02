ba0ee2f7ce42a06a4be62970cb71be0a677066f0
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-02 08:41:53 +0200

    Added another exc

---
+++ Exc/Chapter 5/Excercises/Exconsole2/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write me a sentence, whith a lot of negatives 'NO': ");
+        string input = Console.ReadLine();
+
+        string output = input.Replace("No", "Yes");
+        Console.WriteLine($"Here is the modified sentence: {output}");
+        Console.ReadKey();        
+    }
+}
