6b5f6ff8f504f6d33d5b9e292370a63fd3faab0f
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-02 08:42:11 +0200

    added another

--- Exc/Chapter 5/Ch5Ex5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Exc/Chapter 5/Ch5Ex5/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe078b1b1e83e44b734709f52d13837dcec1e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240662f3f1fc9d411172d5f7abc162fbbf71891c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
