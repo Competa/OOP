f02dc40a33e5b93d34a817ed4bb7e4a8050b83c5
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-03 23:57:13 +0200

    test 2

---
+++ Exc/Chapter 5/Excercises/Exconsole3/ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main (string[] args)
+        {
+            Console.WriteLine("Give me a sentence, and I will identify each word: ");
+            string input = Console.ReadLine();
+
+
+        }
+    
+    static string 
+
+}
