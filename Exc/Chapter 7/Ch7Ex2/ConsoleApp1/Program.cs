c8039573a50f1d460c0d25f281ce2949ed12b826
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-05 18:37:27 +0200

    Added Chapter 7 to the repo

---
+++ Exc/Chapter 7/Ch7Ex2/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input age and gender to complete the sentence: ");
+        Console.WriteLine("Hi, I am [X] years old and I identify as [Y]!");
+        string age = Console.ReadLine();
+        string gender=Console.ReadLine();
+
+        Console.WriteLine($"Hi, I am {age} years old and I identify as {gender}");
+    }
+}
