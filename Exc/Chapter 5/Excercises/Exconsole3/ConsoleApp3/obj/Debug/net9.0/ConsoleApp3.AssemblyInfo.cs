4119159b84dc5d3539be36b2c8607cb7ab62119d
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-05 19:45:40 +0200

    Added.

--- Exc/Chapter 5/Excercises/Exconsole3/ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
+++ Exc/Chapter 5/Excercises/Exconsole3/ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8039573a50f1d460c0d25f281ce2949ed12b826")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aabdeae719dd834f54623e4458fc719a972c17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exc/Chapter 7/Ch7Ex2/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Exc/Chapter 7/Ch7Ex2/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e8761c57ddcc1c6a8ea3771cdfc20784bc4cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aabdeae719dd834f54623e4458fc719a972c17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
