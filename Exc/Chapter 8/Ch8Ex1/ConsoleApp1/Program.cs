642a30da3066626023cff398f77611d152609348
Nicholaj Ghabour <competa123@hotmail.com>
2025-05-11 00:30:12 +0200

    Started Chapter 8

---
+++ Exc/Chapter 8/Ch8Ex1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿partial class Program
+{
+    public static void EnjoyDrink(object cup)
+    {
+        if (cup is HotDrink drink && cup is ICup washable)
+        {
+            drink.AddMilk();
+            drink.Drink();
+            washable.Wash();
+        }
+        else
+        {
+            throw new ArgumentException("Object must be a HotDrink and implement ICup.");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Example usage
+        var myCup = new MyHotDrinkCup();
+        EnjoyDrink(myCup);
+    }
+}
+
+interface ICup
+{
+    void Wash();
+}
+
+class HotDrink
+{
+    public void AddMilk() { /* Implementation */ }
+    public void Drink() { /* Implementation */ }
+}
+
+class MyHotDrinkCup : HotDrink, ICup
+{
+    public void Wash() { /* Implementation */ }
+}
